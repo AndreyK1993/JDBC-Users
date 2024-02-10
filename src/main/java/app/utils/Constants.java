f96ef75e6352059673e0b44159d1eec29f2c5ae3
AndreyK1993 <andrey.kurilenko27@gmail.com>
2024-02-10 17:46:18 +0200

    changed ID_RGX

--- src/main/java/app/utils/Constants.java
+++ src/main/java/app/utils/Constants.java
@@ -10,12 +10,8 @@ public final class Constants {
     public final static String EMAIL_RGX = "^[a-zA-Z0-9_!#$%&’*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$";
     public final static String WRONG_EMAIL_MSG = "Wrong email input.";
 
-    // Regex для id.
-    // Регулярний вираз для позитивних цілих чисел без нулів, мінусів або ком на початку.
-    // ^ вказує на початок рядка
-    // [1-9] відповідає будь-якій цифрі від 1 до 9 і гарантує відсутність нулів на початку.
-    // $ вказує на кінець рядка
-    public final static String ID_RGX = "^[1-9]$";
+    public final static String ID_RGX = "^[1-9][0-9]*$";
+
     public final static String WRONG_ID_MSG = "Wrong id input.";
 
     public final static String DATA_ABSENT_MSG = "\n>> No data!";
